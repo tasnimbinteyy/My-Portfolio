47136a468e7879e437eb8dcb4fbd05e65b9de742
Tushar Barua <tusharbarua269@gmail.com>
2025-12-24 16:35:58 +0600

    Ready for deployment

---
+++ src/app/_not-found/page.tsx
@@ -0,0 +1,10 @@
+// app/_not-found/page.tsx
+"use client";
+
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold text-red-500">404 - Page Not Found</h1>
+    </div>
+  );
+}
